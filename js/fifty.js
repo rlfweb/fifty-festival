5057820caf9a12d68753558732d677bd25f8e692
rlfweb <57272328+rlfweb@users.noreply.github.com>
2021-04-15 09:34:49 +0100

    animation the circles and squiggles

--- js/fifty.js
+++ js/fifty.js
@@ -1,3 +1,4 @@
+// MARQUEE TEXT
 // make a function called makeMarquee
 function makeMarquee() {
   const title = "FIFTY Music Festival — November 10–12, Desert Valley";
@@ -13,3 +14,64 @@ function makeMarquee() {
 
 // run the makeMarquee function
 makeMarquee();
+
+// A function to generate some randomness
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// ANIMATION OF CIRCLES
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
+
+// grab all the .circles from the html
+const circles = document.querySelectorAll(".circle");
+
+// circles return us an array so we need to loop through it
+// inside of the forEach we get access to each individual element
+// along with its index number within the array
+circles.forEach(function (circle, index) {
+  // in here we get access to each one as a 'circle'
+  circle.animate(
+    [
+      // keyframes
+      { transform: "scale(1)" },
+      { transform: "scale(1.2)" },
+      { transform: "scale(1)" },
+    ],
+    {
+      // timing options
+      // we use index to create a staggered animation delay
+      delay: 300 * index,
+      duration: 3000,
+      iterations: Infinity,
+    }
+  );
+});
+
+// ANIMATION OF SQUIGGLES
+
+const squiggles = document.querySelectorAll(".squiggle");
+
+squiggles.forEach((squiggle, index) => {
+  // want a random number between 0 and 45
+  const randomNumber = random(0, 45);
+  console.log(randomNumber);
+  // create a bit of randomness for animation delay
+
+  squiggle.animate(
+    [
+      // keyframes
+      { transform: "rotate(0deg)" },
+      // join randomNumber into our rotate property
+      { transform: `rotate(${randomNumber}deg)` },
+      { transform: "rotate(0deg)" },
+    ],
+    {
+      // timing options
+      // we use index to create a staggered animation delay
+      delay: 300 * index,
+      duration: 5000,
+      iterations: Infinity,
+    }
+  );
+});
